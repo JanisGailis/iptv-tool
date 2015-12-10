e07b85fb8ea04743e1fafcea5335c72078ccc8af
Janis Gailis <janis.gailis@yahoo.com>
2015-12-10 23:55:11 +0100

    Added json based settings. Using these settings already in the event handler.

--- event_handler.py
+++ event_handler.py
@@ -3,13 +3,16 @@ Handle the events captured by the main module.
 """
 
 from evdev import categorize, ecodes
+import json
 
 class EventHandler(object):
     """
     Handles the captured events.
     """
     def __init__(self):
-        pass
+        # Open settings
+        with open("settings.json") as settings_file:
+            self.settings = json.load(settings_file)
 
     def handle(self, event):
         """
@@ -17,6 +20,47 @@ class EventHandler(object):
         """
         if not self.filter_event(event):
             return None
+
+        key = ecodes.KEY[event.code][4:]
+        
+        try:
+            int(key)
+            # Call a 'set channel number' function
+            #return None
+        except ValueError:
+            pass
+
+        # TODO: Read PEP 3103 to figure out how to do this best
+
+        if key == self.settings["start_tv_key"]:
+            # Call 'switch on' function
+            #return None
+            pass
+
+        if key == self.settings["stop_tv_key"]:
+            # Call 'switch off' function
+            #return None
+            pass
+
+        if key == self.settings["channel_down"]:
+            # Call 'channel down' function
+            # return None
+            pass
+
+        if key == self.settings["channel_up"]:
+            # Call channel up function
+            # return None
+            pass
+
+        if key == self.settings["volume_down"]:
+            # Call volume up function
+            # return None
+            pass
+
+        if key == self.settings["volume_up"]:
+            # Call volume up function
+            # return None
+            pass
 
         self.output(event)
 
@@ -31,6 +75,10 @@ class EventHandler(object):
 
         # Filter out if a key 'down' event.
         if event.value != 0:
+            return False
+
+        # Filter out if a key is not found in settings.json
+        if ecodes.KEY[event.code][4:] not in self.settings["accepted_keys"]:
             return False
 
         return True
